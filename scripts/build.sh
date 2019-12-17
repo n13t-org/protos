ed1c52326a95b8135d30b88a78298c54a7baa79a
Shirou <nghinhut@gmail.com>
2019-12-17 14:39:17 +0700

    feat(idm protos): add idm proto files

---
+++ scripts/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+PROTOS=$(find . -iname '*.proto')
+rm -rf static/
+for proto in $PROTOS; do
+  proto=$(echo $proto | cut -d '/' -f2-)
+  dirname=$(dirname $proto)
+  filename=$(basename $proto)
+  printf "./scripts/protoc-all -i %s -f %s -l go -o static/go/$dirname\n" $dirname $filename $outdir
+  eval $(printf "./scripts/protoc-all -i %s -f %s -l go -o static/go/$dirname\n" $dirname $filename $outdir)
+done
+sudo chown -R $USER static
