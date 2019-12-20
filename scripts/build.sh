cc89d85418800ad101388dc12482b7d45657e6c4
Shirou <nghinhut@gmail.com>
2019-12-20 14:01:02 +0700

    docs(idm): add godoc

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,30 @@
 #!/bin/sh
 PROTOS=$(find . -iname '*.proto')
-rm -rf static/
-for proto in $PROTOS; do
-  proto=$(echo $proto | cut -d '/' -f2-)
-  dirname=$(dirname $proto)
-  filename=$(basename $proto)
-  printf "./scripts/protoc-all -i %s -f %s -l go -o generated/$dirname\n" $dirname $filename $outdir
-  eval $(printf "./scripts/protoc-all -i %s -f %s -l go -o generated/$dirname\n" $dirname $filename $outdir)
+OUTPUT_DIR="generated"
+
+rm -rf $OUTPUT_DIR
+# gen by dir
+dir=$(find . -name 'v[0-9]*' -not -path "*.git/*")
+
+for d in $dir; do
+  d=$(echo $d | cut -d '/' -f2-)
+  printf "Generating code in $d, output: $OUTPUT_DIR/$d\n"
+  eval $(printf "./scripts/protoc-all -d $d -l go --with-docs -o $OUTPUT_DIR/$d\n")
 done
-sudo chown -R $USER generated
+
+# gen file by file
+#for proto in $PROTOS; do
+#  proto=$(echo $proto | cut -d '/' -f2-)
+#  dirname=$(dirname $proto)
+#  filename=$(basename $proto)
+#  outdir="generated/$dirname"
+#  printf "./scripts/protoc-all -i %s -f %s -l go --with-docs markdown -o %s\n" $dirname $filename $OUTPUT_DIR/$dirname
+#  eval $(printf "./scripts/protoc-all -i %s -f %s -l go -o %s --with-docs html,%s.html\n" $dirname $filename $OUTPUT_DIR/$dirname $filename)
+#done
+
+
+
+
+
+## Keep this line at the bottom
+[ -d $OUTPUT_DIR ] && sudo chown -R $USER $OUTPUT_DIR
